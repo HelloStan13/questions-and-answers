f72e40e6a144c0908a4934b8bfa090138c6cc708
Adryan <adryanynfante@gmail.com>
2022-06-08 17:50:31 -0500

    conexion firebase
    creacion de usuario e inicio o entrada al sistema

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyA4vxKfZCl1mLLZUvwPEat19Mq694GIW4U",
-    authDomain: "sofkau-reto.firebaseapp.com",
-    projectId: "sofkau-reto",
-    storageBucket: "sofkau-reto.appspot.com",
-    messagingSenderId: "102782956929",
-    measurementId: "G-MX6F56NV8R"
+    apiKey: "AIzaSyBNJi20rFfv81no5l99AFImQS20_a0_LCg",
+    authDomain: "fir-83199.firebaseapp.com",
+    projectId: "fir-83199",
+    storageBucket: "fir-83199.appspot.com",
+    messagingSenderId: "1097598825117",
+    appId: "1:1097598825117:web:0b5b4b34c8e1abc3815a74"
   }
 
   
