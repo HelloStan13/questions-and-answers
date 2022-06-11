ce04ee22042bfe409f9cd9609a7ab1a98ea112f2
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-06-11 12:40:21 -0500

    Prueba AddAnswerUseCase
    Se realiza la prueba del caso de uso add answer

--- src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
@@ -1,7 +1,71 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@SpringBootTest
 class AddAnswerUseCaseTest {
 
+    @SpyBean
+    private AddAnswerUseCase addAnswerUseCase;
+
+    @SpyBean
+    private GetUseCase getUseCase;
+
+    @Autowired
+    private MapperUtils mapperUtils;
+
+    @MockBean
+    AnswerRepository answerRepository;
+
+    @Test
+    void addAnswerHappyPass() {
+        Question question = new Question();
+        QuestionDTO questionDTO = new QuestionDTO("12334", "asdf", "Cuanto es 2 + 2", "Matematicas", "Operaciones");
+        question.setId(questionDTO.getId());
+        question.setUserId(questionDTO.getUserId());
+        question.setQuestion(questionDTO.getQuestion());
+        question.setType(questionDTO.getType());
+        question.setCategory(questionDTO.getCategory());
+
+        List<AnswerDTO> answersDTO = new ArrayList<>();
+
+        Answer answer = new Answer();
+        AnswerDTO answerDTO = new AnswerDTO("asdf", "12334", "Es 4", LocalDateTime.of(2022, 06, 10, 8, 0));
+        answer.setQuestionId(answerDTO.getQuestionId());
+        answer.setUserId(answerDTO.getUserId());
+        answer.setAnswer(answerDTO.getAnswer());
+        answer.setCreateAt(answerDTO.getCreateAt());
+
+        answersDTO.add(answerDTO);
+
+        questionDTO.setAnswers(answersDTO);
+
+        Mockito.when(getUseCase.apply(answerDTO.getQuestionId())).thenReturn(Mono.just(questionDTO));
+        Mockito.when(answerRepository.save(any())).thenReturn(Mono.just(mapperUtils.mapperToAnswer().apply(answerDTO)));
+
+        var result= addAnswerUseCase.apply(answerDTO);
+
+        StepVerifier
+                .create(result)
+                .expectNext(questionDTO)
+                .verifyComplete();
+    }
 }
