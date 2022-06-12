a57f08f5af2435194b21665900ac71f4f0158787
Adryan <adryanynfante@gmail.com>
2022-06-12 16:39:42 -0500

    tests eddit answer
    test ok

--- web/src/app/paginas/edit-answer/edit-answer.component.spec.ts
+++ web/src/app/paginas/edit-answer/edit-answer.component.spec.ts
@@ -1,4 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { MessageService } from 'primeng/api';
+import { QuestionService } from 'src/app/Service/question.service';
+import { ServiceService } from 'src/app/Service/service.service';
+import { environment } from 'src/environments/environment';
 
 import { EditAnswerComponent } from './edit-answer.component';
 
@@ -8,7 +18,9 @@ describe('EditAnswerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditAnswerComponent ]
+      imports: [RouterTestingModule, ReactiveFormsModule, HttpClientModule, ToastrModule.forRoot(), AngularFireModule.initializeApp(environment.firebaseConfig)],
+      declarations: [ EditAnswerComponent ],
+      providers: [MessageService, ServiceService, QuestionService, ToastrService, NgbModal]
     })
     .compileComponents();
   });
